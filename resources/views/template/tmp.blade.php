fed6eb8401cec9cfa29b20a7f172cb6b2ef229f6
afzaal-ahmed0036 <afzaal.ahmed0036@gmail.com>
2023-12-15 17:12:06 +0500

    new

--- resources/views/template/tmp.blade.php
+++ resources/views/template/tmp.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <!-- Mirrored from themesbrand.com/skote-django/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 16 May 2021 18:20:42 GMT -->
+
 <head>
     <meta charset="utf-8" />
     <title>@yield('title')</title>
@@ -10,55 +11,66 @@
     <meta content="Themesbrand" name="author" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{URL('/')}}/assets/images/favicon.ico">
+    <link rel="shortcut icon" href="{{ URL('/') }}/assets/images/favicon.ico">
 
-    <link href="{{asset('assets/libs/fullcalendar/core/main.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/libs/fullcalendar/daygrid/main.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/libs/fullcalendar/bootstrap/main.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/libs/fullcalendar/timegrid/main.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/libs/fullcalendar/core/main.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/libs/fullcalendar/daygrid/main.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/libs/fullcalendar/bootstrap/main.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/libs/fullcalendar/timegrid/main.min.css') }}" rel="stylesheet" type="text/css" />
 
-    <link href="{{asset('assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
-    <link href="{{asset('assets/libs/spectrum-colorpicker2/spectrum.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('assets/libs/bootstrap-timepicker/css/bootstrap-timepicker.min.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css')}}" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="{{asset('assets/libs/chenfengyuan/datepicker/datepicker.min.css')}}">
+    <link href="{{ asset('assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet"
+        type="text/css">
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"
+        rel="stylesheet" />
+    <link href="{{ asset('assets/libs/spectrum-colorpicker2/spectrum.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/libs/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet"
+        type="text/css">
+    <link href="{{ asset('assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link rel="stylesheet" href="{{ asset('assets/libs/chenfengyuan/datepicker/datepicker.min.css') }}">
 
     <!-- Bootstrap Css -->
-    <link href="{{URL('/')}}/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="{{ URL('/') }}/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet"
+        type="text/css" />
     <!-- Icons Css -->
-    <link href="{{URL('/')}}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ URL('/') }}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="{{URL('/')}}/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <link href="{{ URL('/') }}/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
 
-    <link href="{{URL('/')}}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{URL('/')}}/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ URL('/') }}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ URL('/') }}/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css"
+        rel="stylesheet" type="text/css" />
 
     <!-- flatpickr css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/flatpickr.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/themes/light.css">
 
     <!-- Responsive datatable examples -->
-    <link href="{{URL('/')}}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ URL('/') }}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css"
+        rel="stylesheet" type="text/css" />
 
     <!-- Sweet Alert-->
-    <link href="{{URL('/')}}/assets/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ URL('/') }}/assets/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
 
-    <link href="{{URL('/')}}/assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css">
-    <!-- <link rel="stylesheet" href="{{URL('/')}}/assets/libs/%40chenfengyuan/datepicker/datepicker.min.css"> -->
+    <link href="{{ URL('/') }}/assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet"
+        type="text/css">
+    <!-- <link rel="stylesheet" href="{{ URL('/') }}/assets/libs/%40chenfengyuan/datepicker/datepicker.min.css"> -->
 
-    <link rel="stylesheet" type="text/css" href="{{URL('/')}}/assets/libs/toastr/build/toastr.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ URL('/') }}/assets/libs/toastr/build/toastr.min.css">
     <!-- Responsive datatable examples -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-
-    alpha/css/bootstrap.css" rel="stylesheet">
+    alpha/css/bootstrap.css"
+        rel="stylesheet">
 
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-   <link rel="stylesheet" type="text/css"
-    href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 
     @stack('before-styles')
@@ -67,7 +79,8 @@
 
     @yield('page-styles')
     <!-- my own model -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -112,53 +125,14 @@
         <!-- end of footer -->
 
         <!-- JAVASCRIPT -->
-        <script src="{{URL('/')}}/assets/libs/jquery/jquery.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <!-- <script src="{{URL('/')}}/assets/libs/bootstrap/js/bootstrap.min.js"></script> -->
-        <script src="{{URL('/')}}/assets/libs/metismenu/metisMenu.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/simplebar/simplebar.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/node-waves/waves.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/jquery/jquery.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- <script src="{{ URL('/') }}/assets/libs/bootstrap/js/bootstrap.min.js"></script> -->
+        <script src="{{ URL('/') }}/assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/node-waves/waves.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/flatpickr.js"></script>
-
-
         <script>
-            @if(Session::has('message'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true
-            }
-                    toastr.success("{{ session('message') }}");
-            @endif
-
-            @if(Session::has('error'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true
-            }
-                    toastr.error("{{ session('error') }}");
-            @endif
-
-            @if(Session::has('info'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true
-            }
-                    toastr.info("{{ session('info') }}");
-            @endif
-
-            @if(Session::has('warning'))
-            toastr.options =
-            {
-                "closeButton" : true,
-                "progressBar" : true
-            }
-                    toastr.warning("{{ session('warning') }}");
-            @endif
-          </script>
-          <script>
             @if (Session::has('message'))
                 toastr.options = {
                     "closeButton": true,
@@ -201,43 +175,43 @@
             @endif
         </script>
         <!-- form mask init -->
-        <script src="{{URL('/')}}/assets/js/pages/form-mask.init.js"></script>
+        <script src="{{ URL('/') }}/assets/js/pages/form-mask.init.js"></script>
 
 
         <!-- App js -->
-        <script src="{{URL('/')}}/assets/js/app.js"></script>
+        <script src="{{ URL('/') }}/assets/js/app.js"></script>
 
         <!-- form mask -->
-        <script src="{{URL('/')}}/assets/libs/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/inputmask/min/jquery.inputmask.bundle.min.js"></script>
 
         <!-- form mask init -->
 
         <!-- Required datatable js -->
-        <script src="{{URL('/')}}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
         <!-- Buttons examples -->
-        <script src="{{URL('/')}}/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
         <!-- toastr plugin -->
-        <script src="{{URL('/')}}/assets/libs/toastr/build/toastr.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/toastr/build/toastr.min.js"></script>
 
         <!-- toastr init -->
-        <script src="{{URL('/')}}/assets/js/pages/toastr.init.js"></script>
+        <script src="{{ URL('/') }}/assets/js/pages/toastr.init.js"></script>
 
         <!-- form repeater js -->
-        <script src="{{asset('assets/libs/jquery.repeater/jquery.repeater.min.js')}}"></script>
+        <script src="{{ asset('assets/libs/jquery.repeater/jquery.repeater.min.js') }}"></script>
 
-        <script src="{{asset('assets/js/pages/form-repeater.int.js')}}"></script>
+        <script src="{{ asset('assets/js/pages/form-repeater.int.js') }}"></script>
 
 
         <!-- Required datatable js -->
         <!-- Buttons examples -->
-        <script src="{{URL('/')}}/assets/libs/jszip/jszip.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/pdfmake/build/pdfmake.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/pdfmake/build/vfs_fonts.js"></script>
-        <script src="{{URL('/')}}/assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/jszip/jszip.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/pdfmake/build/pdfmake.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/pdfmake/build/vfs_fonts.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
 
         <!-- Responsive examples -->
 
@@ -245,41 +219,43 @@
 
         <!-- apexcharts -->
 
-        <script src="{{URL('/')}}/assets/libs/select2/js/select2.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/select2/js/select2.min.js"></script>
 
         <!-- init js -->
-        <script src="{{URL('/')}}/assets/js/pages/ecommerce-select2.init.js"></script>
+        <script src="{{ URL('/') }}/assets/js/pages/ecommerce-select2.init.js"></script>
 
         <!-- Sweet Alerts js -->
-        <script src="{{URL('/')}}/assets/libs/sweetalert2/sweetalert2.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/sweetalert2/sweetalert2.min.js"></script>
 
         <!-- Sweet alert init js-->
-        <script src="{{URL('/')}}/assets/js/pages/sweet-alerts.init.js"></script>
+        <script src="{{ URL('/') }}/assets/js/pages/sweet-alerts.init.js"></script>
 
         <!-- form repeater js -->
 
 
-        <script src="{{URL('/')}}/assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
-        <!-- <script src="{{URL('/')}}/assets/libs/%40chenfengyuan/datepicker/datepicker.min.js"></script> -->
+        <script src="{{ URL('/') }}/assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+        <!-- <script src="{{ URL('/') }}/assets/libs/%40chenfengyuan/datepicker/datepicker.min.js"></script> -->
 
-        <script src="{{URL('/')}}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-        <script src="{{URL('/')}}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="{{ URL('/') }}/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
         <!-- Datatable init js -->
-        <script src="{{URL('/')}}/assets/js/pages/datatables.init.js"></script>
+        <script src="{{ URL('/') }}/assets/js/pages/datatables.init.js"></script>
 
 
-        <script src="{{asset('assets/libs/moment/min/moment.min.js')}}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-        <script src="{{asset('assets/libs/jquery-ui-dist/jquery-ui.min.js')}}"></script>
-       <!-- <script src="{{asset('assets/libs/fullcalendar/core/main.min.js')}}"></script>
-        <script src="{{asset('assets/libs/fullcalendar/bootstrap/main.min.js')}}"></script>
-        <script src="{{asset('assets/libs/fullcalendar/daygrid/main.min.js')}}"></script>
-        <script src="{{asset('assets/libs/fullcalendar/timegrid/main.min.js')}}"></script>
-        <script src="{{asset('assets/libs/fullcalendar/interaction/main.min.js')}}"></script>
+        <script src="{{ asset('assets/libs/moment/min/moment.min.js') }}"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js">
+        </script>
+        <script src="{{ asset('assets/libs/jquery-ui-dist/jquery-ui.min.js') }}"></script>
+        <!-- <script src="{{ asset('assets/libs/fullcalendar/core/main.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/fullcalendar/bootstrap/main.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/fullcalendar/daygrid/main.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/fullcalendar/timegrid/main.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/fullcalendar/interaction/main.min.js') }}"></script>
 
          Calendar init
-        <script src="{{asset('assets/js/pages/calendars-full.init.js')}}"></script> -->
+        <script src="{{ asset('assets/js/pages/calendars-full.init.js') }}"></script> -->
 </body>
 @stack('before-scripts')
 <script>
@@ -311,7 +287,7 @@
     function delete_confirm(url_plus_id) {
         var url;
 
-        url = "{{URL::TO('/delete_customer')}}" + '/' + url_plus_id;
+        url = "{{ URL::TO('/delete_customer') }}" + '/' + url_plus_id;
         jQuery('#staticBackdrop').modal('show', {
             backdrop: 'static'
         });
@@ -320,7 +296,7 @@
     }
 
     function delete_confirm2(url, id) {
-        url = "{{URL::TO('/')}}/" + url + '/' + id;
+        url = "{{ URL::TO('/') }}/" + url + '/' + id;
         // console.log(url);return
 
         jQuery('#staticBackdrop').modal('show', {
